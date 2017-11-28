0bece80fb87c2ea9e11731c14655197292b85cd9
mchorney <m.chorney7@gmail.com>
2017-11-28 17:05:41 -0600

    added basics Python assignments

---
+++ Basics/multiplication_table.py
@@ -0,0 +1,13 @@
+def multiplication_table():
+
+    tableString = "x"
+    for x in range(1, 13):
+        tableString += "  " + str(x)
+    print tableString
+    for j in range(1, 13):
+        tableString = str(j)
+        for i in range(1, 13):
+            tableString += "  " + str(i * j)
+        print tableString
+
+multiplication_table()
